476d91ee74b7dc3a23fa1808d36adb416af1f800
yuunlimm <dla0267@gmail.com>
2024-09-29 00:25:47 -0700

    cargo build pass

--- demo/target/debug/deps/demo-43e839ee2ab171bc.d
+++
@@ -1,5 +0,0 @@
-/Users/yuunlim/test-workflow/demo/target/debug/deps/demo-43e839ee2ab171bc: src/main.rs
-
-/Users/yuunlim/test-workflow/demo/target/debug/deps/demo-43e839ee2ab171bc.d: src/main.rs
-
-src/main.rs:
--- demo/target/debug/deps/demo-5bf7dcc9b30b010d.d
+++
@@ -1,5 +0,0 @@
-/Users/yuunlim/test-workflow/demo/target/debug/deps/demo-5bf7dcc9b30b010d: src/main.rs
-
-/Users/yuunlim/test-workflow/demo/target/debug/deps/demo-5bf7dcc9b30b010d.d: src/main.rs
-
-src/main.rs:
